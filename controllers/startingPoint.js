18299d0fab06edaba4746bbc5d8523206c7ff3fd
virtual-luna <maya.ellington@berkeley.edu>
2022-04-14 17:12:25 -0700

    edit/update complete

--- controllers/startingPoint.js
+++ controllers/startingPoint.js
@@ -18,59 +18,41 @@ function deleteTrip(req, res){
   )
 }
 
-function edit(req, res) {
-  StartingPoint.findOne(
-    { _id: req.params.id},
+function edit(req, res, next) {
+  StartingPoint.findById(
+    req.params.id,
     function (err, tripDb) {
-      if (err || !tripDb) return res.redirect(`/${StartingPoint._id}`); //unsure of redirect
-      res.render(`allTrips/${StartingPoint._id}/edit`, { tripDb });
+      console.log(tripDb)
+      // const startEnd = tripDb.tripStartEnd.id(req.params.id)
+      if (!tripDb.user.equals(req.user._id)) return res.redirect(`allTrips/${StartingPoint._id}`); //unsure of redirect
+      res.render('startingPoint/edit', { tripDb, startingPointId: req.params.id });
     }
   );
 }
 
 function update(req, res) {
   StartingPoint.findOneAndUpdate(
-    { _id: req.params.id, user: req.user._id },
+    req.params.id,
     req.body,
     { new: true },
     function (err, trip) {
+      console.log(trip, 'trip')
       if (err || !trip) return res.redirect(`/${StartingPoint._id}`); //unsure of redirect
-      res.redirect(`${StartingPoint._id}/${trip._id}`);
+      res.redirect(`/allTrips/${trip._id}`);
     }
   );
 }
 
-//adjust so index only shows
+//Index show for all trips from given location starting point
 function index(req, res) {
   StartingPoint.find({'tripBegin': req.params.id}, function (err, trips) {
-    // const tripBeginsId = req.params.id;
-    console.log(req.params.id, 'req.params.id<-----')
-    console.log(trips, 'trip<-----')
-    
-    // console.log(trips.tripBegin.id(req.params.id))
+
     res.render("startingPoint/index", {
       trips,
-      // tripId,
       title: "All Road Trips",
     });
   });
 }
-
-  // function show(req, res) {
-  //   TripBegin.find({tripBegin})
-  //   .populate('trip')
-  //   .exec(function(err, trip) {
-  //     // Native MongoDB approach
-  //     Trip.find({},
-  //      function(err, trip) {
-  //        console.log(trip);
-  //        res.render('trips/show', {
-  //          title: 'Trip Detail', trip
-  //        });
-  //      }
-  //    );
-  //   }
-  // }
 
 function newTrip(req, res) {
   StartingPoint.find({}, function (err, trip) {
@@ -86,21 +68,10 @@ function newTrip(req, res) {
 async function create(req, res) {
   req.body.tripBegin = req.params.id; 
 
-  
-  // TripBegin.findById(req.params.id, function(err, startPoint) {
-    // console.log(startingPoint)
-    // console.log(startPoint)
-
-    // startPoint.tripDetail.push(startingPoint);
-    // console.log(startPoint)
     req.body.user = req.user._id;
 		req.body.userName = req.user.name;
 		req.body.userAvatar = req.user.avatar;
     
     const startingPoint = await StartingPoint.create(req.body);
- 
-    console.log(startingPoint)
-    // startPoint.save(function (err) {
-      // mongoose talking
       res.redirect(`/allTrips/${startingPoint._id}`);
   };
