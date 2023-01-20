b17bff2d687a063363fc60ad1d14c1fab0571134
virtual-luna <maya.ellington@berkeley.edu>
2023-01-20 15:59:22 -0800

    configured cyclic deployment code

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const MongoStore = require('connect-mongo');
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
@@ -22,7 +23,6 @@ const tripsBeginRouter = require('./routes/tripsBegin');
 const allTripsRouter = require('./routes/allTrips');
 const startingPointRouter = require('./routes/startingPoint');
 
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -34,15 +34,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 // mount the session middleware
-app.use(session({
-  secret: process.env.SECRET,
-  resave: false,
-  saveUninitialized: true
-}));
+app.use(
+  session({
+    store: MongoStore.create({
+      mongoUrl: process.env.DATABASE_URL,
+    }),
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
-
 
 // Add this middleware BELOW passport middleware
 app.use(function (req, res, next) {
@@ -53,12 +57,12 @@ app.use(function (req, res, next) {
 
 // mount all routes with appropriate base paths
 app.use('/', indexRouter);
-app.use('/api', tripsBeginRouter); 
+app.use('/api', tripsBeginRouter);
 app.use('/allTrips', allTripsRouter);
 app.use('/', startingPointRouter);
 
 // invalid request, send 404 page
-app.use(function(req, res) {
+app.use(function (req, res) {
   res.status(404).send('Cant find that!');
 });
 
